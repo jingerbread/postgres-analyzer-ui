c79af26ad8a0fcaa29a9162fc57339ba5f2e3aa5
jingerbread <s137948>
2018-01-29 11:40:55 +0300

    [fixed] )

--- src/app/home.service.ts
+++ src/app/home.service.ts
@@ -34,7 +34,7 @@ export class HomeService {
 
     performAnalysis(analysisId: String): Observable<AnalysisResult> {
 
-        return this.http.post<AnalysisResult>( this.performAnalysisUrl + 'CQxRC'/*TODO: analysisId*/, null,
+        return this.http.post<AnalysisResult>( this.performAnalysisUrl + '1vINJ'/*TODO: analysisId*/, null,
             {headers: new HttpHeaders()
                     .append('Accept', 'application/json')
                     .append('Content-Type', 'application/json')});
